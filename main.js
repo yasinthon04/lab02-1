9ec36401eb98c8129ed451faaa128c41cc966131
chartchai <chartchai.d@cmu.ac.th>
2021-07-08 16:26:06 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Shoes',
+            image: './assets/images/socks_green.jpg',
+            inStock: true,
+            inventory: 100,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+            ],
+            cart: 0
+
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
+        }
+    }
+})
