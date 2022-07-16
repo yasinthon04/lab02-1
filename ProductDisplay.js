148ef978ba0a1abc3ad62eccf6ab720c5cd14615
Yasinthon <yasinthon_r@cmu.ac.th>
2022-07-16 18:52:07 +0700

    4.3

--- ProductDisplay.js
+++ ProductDisplay.js
@@ -27,4 +27,43 @@ app.component ('product-display',{
             </div>
         </div>
     </div>`
+    ,
+    data() {
+        return {
+            product: 'Shoes',
+            brand: 'SE 331',
+
+            inventory: 100,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
+            ],
+            activeClass: true,
+            selectedVariant:0,
+
+        }
+    },
+     methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
+        },
+        updateVariant(index) {
+            this.selectedVariant = index;
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        }
+    }
 }) 
