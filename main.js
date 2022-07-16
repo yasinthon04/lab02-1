a1fafef17ceda33b9d9b6fc72e7ada642706ad87
Yasinthon <yasinthon_r@cmu.ac.th>
2022-07-16 16:45:00 +0700

    2.4

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = Vue.createApp({
         return {
             product: 'Shoes',
             image: './assets/images/socks_green.jpg',
-            inStock: true,
+            inStock: false,
             inventory: 100,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
