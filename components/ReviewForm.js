f0cc8ecf66f9c53c05ff75a9c38e4db7866a1376
Yasinthon <yasinthon_r@cmu.ac.th>
2022-07-16 20:37:05 +0700

    6.3

--- components/ReviewForm.js
+++ components/ReviewForm.js
@@ -4,12 +4,12 @@ app.component('review-form', {
     `<form class="review-form">
     <h3>Leave a review</h3>
     <label for="name">Name:</label>
-    <input id="name">
+    <input id="name" v-model="name">
     
     <label for="review">Review:</label>
-    <textarea id="review"></textarea>
+    <textarea id="review" v-model="review"></textarea>
     <label for="rating">Rating:</label>
-    <select id="rating">
+    <select id="rating" v-model.number="rating">
         <option>5</option>
         <option>4</option>
         <option>3</option>
